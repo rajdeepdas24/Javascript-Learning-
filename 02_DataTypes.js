f399008b05ec00a65dc30b493ea351d0c7012060
Rajdeep Das <rajdeepdasofficial24@gmail.com>
2026-06-24 20:44:32 +0530

    L5

---
+++ 02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //Treat all JS codes as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+let name = "Rajdeep" 
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
