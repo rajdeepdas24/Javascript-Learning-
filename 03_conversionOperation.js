a184d30556c1cd02dbd27f9dc69614688b4b0a5e
Rajdeep Das <rajdeepdasofficial24@gmail.com>
2026-06-24 21:43:40 +0530

    L6 and L7

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -30,3 +30,49 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// _______operators___________________________________________________________________________________ //
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// basic operators______________________________________
+
+// console.log(2+2); //add
+// console.log(2-2); //sub
+// console.log(2*2); //multiply
+// console.log(2**2); //power
+// console.log(2/2); //divide
+// console.log(2%2); //mod
+
+let str1 = "Hello "
+let str2 = "Rajdeep"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2); //if string is in first, everything will be treated as string 
+// console.log("1" + (2 + 2)); //but if we use brackets, it doesn't behave like string
+// console.log(1 + 2 + "2"); // that's why we need to use proper brackets for readability, but can be asked in interviews or exams
+
+
+
+// console.log(true); 
+// console.log(+true); //gives 1 as output
+// console.log(true+); //This is bullshit
+
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 //not to code like this
+
+
+let gameCounter = 100
+++gameCounter; //pre-increment = 101
+gameCounter++; //post-increment = 102
+// console.log(gameCounter); //102
+
