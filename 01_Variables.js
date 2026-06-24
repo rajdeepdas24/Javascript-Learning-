faeb6f32fde2bc44f3339b0b34c8155b9fa1896e
Rajdeep Das <rajdeepdasofficial24@gmail.com>
2026-06-24 20:44:13 +0530

    L4

---
+++ 01_Variables.js
@@ -0,0 +1,19 @@
+const accountId  = 144553 //constants
+let accountEmail ="rajdeepdas2431@gmail.com"  //Let
+var accountPassword = "12345"
+accountCity = "Silchar"
+let accountState; // value will be undefined
+
+// accountId = 2
+
+accountEmail = "hc@hc@.com"
+accountPassword = "2121212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+ prefer not to use var cuz of issue in block scope and function scope 
+*/
