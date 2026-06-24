538ad6adf3b8384dcee9d9457635ec0dd0ab719c
Rajdeep Das <rajdeepdasofficial24@gmail.com>
2026-06-24 20:44:53 +0530

    L6

---
+++ 03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = 33 //number
+// let score = "33" //string
+// let score = null //NaN , object
+// let score = undefined //NaN , undefined
+// let score = true //1, boolean
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN 
+// true => 1 , false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// true => 1 , false => 0
+// "" => false
+// "Rajdeep" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
