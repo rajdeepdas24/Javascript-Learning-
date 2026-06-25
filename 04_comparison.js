275bb8cdf3c9ec82111ef631407b33e4baaf5627
Rajdeep Das <rajdeepdasofficial24@gmail.com>
2026-06-25 11:09:07 +0530

    L8

---
+++ 04_comparison.js
@@ -0,0 +1,32 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// equality and comparisons works differently
+// comparison converts null to a number, treating it as 0
+// that's why null>=0 is true
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// but it doesn't follow the same with undefined
+
+// === strictly check
+
+console.log("2" === 2);
